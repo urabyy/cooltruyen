a82b7dd5325269168ed66015b611b11286ebfe81
Uraby <thienkhiem.huynh@hotmail.com>
2023-05-08 20:37:03 +0700

    setting database

---
+++ src/main/java/com/uraby/cooltruyen/CooltruyenApplication.java
@@ -0,0 +1,15 @@
+package com.uraby.cooltruyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {ReactiveSecurityAutoConfiguration.class, SecurityAutoConfiguration.class })
+public class CooltruyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooltruyenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uraby/cooltruyen/CooltruyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uraby.cooltruyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooltruyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
